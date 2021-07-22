2455d78063b58f553df00cdc5b73dd5e9075aa10
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-07-22 20:11:10 +0530

    zooms in to user's current location after asking permission & adds a marker on touch

--- app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceConstants.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceConstants.kt
@@ -4,5 +4,6 @@ object GeofenceConstants {
 
     const val GEOFENCE_RADIUS_IN_METERS = 200F
     const val GEOFENCE_EXPIRATION_IN_MILLISECONDS = 20L
+    const val DEFAULT_ZOOM = 15F
 
 }
--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,28 +1,52 @@
 package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
 
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
+import com.udacity.project4.locationreminders.geofence.GeofenceConstants
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 
 class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
+    companion object {
+        private const val TAG = "SelectLocationFragment"
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 222
+    }
+
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
 
     private lateinit var map: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private var locationPermissionGranted = false
+    private var lastKnownLocation: Location?= null
+    private val defaultLocation = LatLng(-33.852, 151.211)
+    private var selectedMarker: Marker? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -39,29 +63,163 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
 
-//        TODO: zoom to the user location after taking his permission
+//        TODO: zoom to the user location after taking her permission
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
 //        TODO: add style to the map
 //        TODO: put a marker to location that the user selected
 
 
 //        TODO: call this function after the user confirms on the selected location
-        onLocationSelected()
+//        onLocationSelected()
 
         return binding.root
     }
 
+    /**
+     * Check whether the user has granted fine location permission.
+     * If not, request the permission
+     */
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
 
+    /**
+     * Handle the result of the permission request
+     */
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        locationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                }
+            }
+        }
+        updateLocationUI()
+    }
+
+    /**
+     * Set up the map when the GoogleMap object is available
+     */
     override fun onMapReady(googleMap: GoogleMap?) {
-        val sydney = LatLng(-33.852, 151.211)
-        googleMap?.addMarker(
-            MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney")
-        )
+        map = googleMap!!
+
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI()
+        if (locationPermissionGranted) {
+            // Get the current location of the device and set the position of the map.
+            getDeviceLocation()
+        }
+
+        map.setOnMapClickListener { latLng ->
+            // Get the current location of the device and set the position of the map.
+            selectedMarker?.remove()
+            selectedMarker?.position = latLng
+            map.addMarker(MarkerOptions().position(latLng)).also {
+                selectedMarker = it
+            }
+            map.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+            onLocationSelected(latLng)
+        }
+
+//        googleMap?.addMarker(
+//            MarkerOptions()
+//                .position(defaultLocation)
+//                .title("Marker in Sydney")
+//        )
+    }
+
+    /**
+     * Set the location controls on the map. If the user has granted location permission,
+     * enable the My Location layer and the related control on the map,
+     * otherwise disable the layer and the control, and set the current location to null.
+     */
+    private fun updateLocationUI() {
+        try {
+            if (locationPermissionGranted) {
+                map.isMyLocationEnabled = true
+                map.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                map.isMyLocationEnabled = false
+                map.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    /**
+     * Use the fused location provider to find the device's last-known location,
+     * then use that location to position the map.
+     */
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation=task.result
+                        if (lastKnownLocation != null) {
+                            Log.d(TAG, "Last known location lat: $lastKnownLocation.latitude, lng: $lastKnownLocation.longitude")
+
+                            map.moveCamera(
+                                CameraUpdateFactory.newLatLngZoom(
+                                    LatLng(
+                                        lastKnownLocation!!.latitude,
+                                        lastKnownLocation!!.longitude
+                                    ), GeofenceConstants.DEFAULT_ZOOM
+                                )
+                            )
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.")
+                            Log.e(TAG, "Exception: %s", task.exception)
+                            map.moveCamera(
+                                CameraUpdateFactory
+                                    .newLatLngZoom(defaultLocation, GeofenceConstants.DEFAULT_ZOOM)
+                            )
+                            map.uiSettings?.isMyLocationButtonEnabled=false
+                        }
+                    } else {
+                        Log.d(TAG, "Couldn't get current location")
+                        Toast.makeText(context, "Unable to get current location", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
     }
 
 
-    private fun onLocationSelected() {
+    private fun onLocationSelected(latLng: LatLng) {
         //        TODO: When the user confirms on the selected location,
         //         send back the selected location details to the view model
         //         and navigate back to the previous fragment to save the reminder and add the geofence
