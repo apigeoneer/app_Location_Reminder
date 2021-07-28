d725c5d0f1a32b9f085b53ba9c94a03561deb507
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-07-28 10:10:52 +0530

    created a fake data source for testing

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -11,6 +11,7 @@ import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -43,6 +44,8 @@ class AuthenticationActivity : AppCompatActivity() {
             if (resultCode == Activity.RESULT_OK) {
                 // User successfully signed in
                 Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+                val intent = Intent(this, RemindersActivity::class.java)
+                startActivity(intent)
 
             } else {
                 // Sign in failed. If response is null the user canceled the
--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,25 +4,45 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var reminders: MutableList<ReminderDTO>?) : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    // Create a fake data source to act as a double to the real data source
+
+    private var shouldReturnError = false
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (shouldReturnError)
+                return Result.Error("Error getting reminders")
+
+        reminders?.let { return Result.Success(ArrayList(it)) }
+
+        return Result.Error("Reminders not found")
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        if (shouldReturnError)
+            return Result.Error("Error getting Reminder")
+
+        reminders?.forEach {
+            return when(id) {
+                it.id -> Result.Success(it)
+                else -> Result.Error("No reminder found with id $id")
+            }
+        }
+
+        return Result.Error("No reminder found with id $id")
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders?.clear()
     }
 
+    fun setReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
 
 }
