1182f1aeb495f6f52ab0047388b07e14ab64098f
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-08-04 21:55:53 +0530

    added tests in RemindersActivityTest

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,16 +1,31 @@
 package com.udacity.project4
 
 import android.app.Application
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.monitorActivity
+import com.udacity.project4.utils.EspressoIdlingResource
 import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -19,14 +34,29 @@ import org.koin.dsl.module
 import org.koin.test.AutoCloseKoinTest
 import org.koin.test.get
 
+
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 //END TO END test to black box test the app
 class RemindersActivityTest :
-    AutoCloseKoinTest() {// Extended Koin Test - embed autoclose @after method to close Koin after every test
+    AutoCloseKoinTest() {                                // Extended Koin Test - embed autoclose @after method to close Koin after every test
 
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    @Before
+    fun registerIdlingResource(): Unit = IdlingRegistry.getInstance().run {
+        register(EspressoIdlingResource.countingIdlingResource)
+        register(dataBindingIdlingResource)
+    }
+
+    @After
+    fun unregisterIdlingResource(): Unit = IdlingRegistry.getInstance().run {
+        unregister(EspressoIdlingResource.countingIdlingResource)
+        register(dataBindingIdlingResource)
+    }
+
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -34,7 +64,6 @@ class RemindersActivityTest :
      */
     @Before
     fun init() {
-        stopKoin()//stop the original app koin
         appContext = getApplicationContext()
         val myModule = module {
             viewModel {
@@ -65,7 +94,41 @@ class RemindersActivityTest :
         }
     }
 
+    @After
+    fun teardown() {
+        stopKoin()                  //stop the original app koin
+    }
 
-//    TODO: add End to End testing to the app
+
+    // End to End testing to the app
+    @Test
+    fun remindersActivity_DisplayedInUi_DisplaysAReminder() {
+        val scenario = ActivityScenario.launch(RemindersActivity::class.java)
+
+        dataBindingIdlingResource.monitorActivity(scenario)
+
+        onView(withId(R.id.noDataTextView)).check(matches(isDisplayed()))
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(typeText("title"))
+        onView(withId(R.id.reminderDescription)).perform(typeText("description"))
+        onView(withId(R.id.selectLocation)).perform(click())
+        onView(withId(R.id.map)).perform(click())
+        Espresso.pressBack()
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        val reminder = ReminderDTO("title", "description", "location",
+            16.78132279413486, 73.35721723965958)
+
+        runBlocking {
+            repository.saveReminder(reminder)
+        }
+
+        onView(withText("title")).check(matches(isDisplayed()))
+        onView(withText("description")).check(matches(isDisplayed()))
+
+        scenario.close()
+
+    }
+
 
 }
